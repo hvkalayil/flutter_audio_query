cf5290684edd89b20690bf72e5b00ad0f75539c2
Hosea Varghese Kalayil <hoseakalayil@gmail.com>
2020-07-05 11:58:22 +0530

    Added set functionality

--- lib/src/song_info.dart
+++ lib/src/song_info.dart
@@ -3,6 +3,13 @@ part of flutter_audio_query;
 /// SongInfo class holds all information about a specific song audio file.
 class SongInfo extends DataModel {
   SongInfo._(Map<dynamic, dynamic> map) : super._(map);
+  
+  //Added set functionality.
+  // Currently only for 3, but can be easily extended
+  void setAlbumTitle(String title) => _data['title'] = title;
+  void setAlbumArt(String path) => _data['album_artwork'] = path;
+  void setAlbumArtist(String artist) => _data['artist'] = artist;
+  //End of changes(by https://hvkalayil.github.io/#/)
 
   /// Returns the album id which this song appears.
   String get albumId => _data['album_id'];
